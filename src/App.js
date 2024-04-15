35f3bcec5e0b6a04a5d05247b9a24bd8327088ed
Naik <Naik@Naik-PC.com>
2024-04-15 20:35:43 +0300

    Added contacts page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Navbar />
 				<Routes>
 					<Route path="/" element={<Home />} />
+					<Route path="/contacts" element={<Contacts />} />
 				</Routes>
         <Footer />
       </Router>
---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,32 @@
+const Contacts = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>Moscow, Russia</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Telegram / WhatsApp</h2>
+            <p>
+              <a href="tel:+79163510160">@nikolay_smhn / +7 (916) 351-01-60</a>
+            </p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>
+              <a href="mailto:nikola-ch2@mail.ru">
+                nikola-ch2@mail.ru
+            </a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default Contacts;
