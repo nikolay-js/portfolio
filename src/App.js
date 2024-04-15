21a2d8e04c6099bb0d8137567c836c3dd802ec96
Naik <Naik@Naik-PC.com>
2024-04-15 18:11:07 +0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./styles/main.css";
 import {BrowserRouter as Router} from "react-router-dom";
 
 import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Footer />
       </Router>
     </div>
   );
