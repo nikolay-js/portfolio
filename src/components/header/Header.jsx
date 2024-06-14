095d4efd921d102f50afd619e3245488945ae145
Naik <Naik@Naik-PC.com>
2024-06-14 13:48:04 +0300

    Added typing effect on header text

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,16 @@ const Header = () => {
           <br />a frontend developer
 				</h1>
         <div className="header__text">
-          <p>with passion for learning and creating.</p>
+          <p>with passion for learning and creating</p>
+          <div className="header__text-container">
+            <div>on</div>
+            <ul className="header__text-dynamic">
+              <li><span>HTML / CSS.</span></li>
+              <li><span>JavaScript.</span></li>
+              <li><span>TypeScript.</span></li>
+              <li><span>React.</span></li>
+            </ul>
+          </div>
         </div>
         <a href="https://github.com/nikolay-js" target="_blank" rel="noreferrer" className="btn">
           <img src={gitHubIcon} alt="GitHubIcon" />
